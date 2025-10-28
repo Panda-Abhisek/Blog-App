8437785ef43f2e8bda46faf57872edb8cd6e2178
Panda__Abhisek <abhisekpanda114@gmail.com>
2025-10-28 12:59:03 +0530

    Enable Async Functionality for Email Service

--- blogapp/src/main/java/com/panda/blogapp/service/impl/EmailService.java
+++ blogapp/src/main/java/com/panda/blogapp/service/impl/EmailService.java
@@ -2,6 +2,7 @@ package com.panda.blogapp.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
